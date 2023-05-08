6ba5fafeb7284337f1ede43da151fdb20a7c8810
noLEAL <83329725+noLEAL@users.noreply.github.com>
2023-05-08 11:00:01 -0300

    ED/2
    primeiro comit do segundo trabalho

--- Estrutura de Dados/Exercício 2/estrutura_OB/src/Main.java
+++ Estrutura de Dados/Exercício 2/estrutura_OB/src/Main.java
@@ -9,20 +9,24 @@ public class Main {
         int opition,size;
         classeVetor myObject;
 
+        System.out.println("Qual tamanho do vetor:");
+        size = key.nextInt();
+        myObject = new classeVetor(size);
+
         while(true) {
 
             System.out.println("=".repeat(100));
 
-            System.out.println("1. Preencher o vetor do objeto com números pseudo-randômicos, em uma distribuição que minimize a possibilidade de valores duplicados.");
-            System.out.println("2. ");
-            System.out.println("3. ");
-            System.out.println("4. ");
-            System.out.println("5. ");
-            System.out.println("6. ");
-            System.out.println("7. ");
-            System.out.println("8. ");
-            System.out.println("9. ");
-            System.out.println("10. ");
+            System.out.println("1. Preencher o vetor do objeto com números pseudo-randômicos, em uma distribuição que minimize a possibilidade de valores duplicados."); //OK
+            System.out.println("2. Preencher o vetor do objeto com números pseudo-randômicos em ordem crescente");
+            System.out.println("3. Informar o total de elementos válidos no vetor do objeto");
+            System.out.println("4. Informar o maior e o menor valores existentes no vetor do objeto");
+            System.out.println("5. Listar todo o conteúdo do vetor do objeto");
+            System.out.println("6. Inserir um valor no vetor do objeto em uma determinada posição.");
+            System.out.println("7. Apagar um valor do vetor do objeto, informando o valor a ser removido");
+            System.out.println("8. Apagar um valor do vetor do objeto, informando a posição do valor a ser removido.");
+            System.out.println("9. Consultar a existência de um valor no vetor do objeto, utilizando busca sequencial");
+            System.out.println("10. Consultar a existência de um valor no vetor do objeto, utilizando busca binária");
             System.out.println("11. ");
             System.out.println("12. ");
             System.out.println("13. ");
@@ -32,11 +36,96 @@ public class Main {
 
             opition = key.nextInt();
 
+
             switch (opition){
                 case 1:
-                    size = key.nextInt();
-                    myObject = new classeVetor(size);
 
+                    myObject.preenchedor_Aleatorio(); // OK
+
+                    break;
+                case 2:
+
+                    myObject.crecente_Aleatorio();
+
+                    break;
+                case 3:
+                    myObject.elementos_Validos();
+
+                    break;
+                case 4:
+
+                    myObject.menor_Maior();
+
+                    break;
+                case 5:
+
+                    myObject.info();
+                    System.out.println("");
+
+                    break;
+                case 6:
+
+                    int valor,stance;
+
+                    System.out.println("Qual valor que deseja inserir:");
+                    valor = key.nextInt();
+                    System.out.println("Em qual indice deseja inserir:");
+                    stance = key.nextInt();
+                    myObject.insert_Valor(valor, stance);
+
+                    break;
+                case 7:
+
+                    int apagar;
+
+                    System.out.println("Qual valor que deseja apagar:");
+                    apagar = key.nextInt();
+                    myObject.apagar_Valor(apagar);
+
+                    break;
+                case 8:
+
+                    int indice;
+
+                    System.out.println("Qual indice que deseja apagar:");
+                    indice = key.nextInt();
+                    myObject.apagar_Indice(indice);
+
+                    break;
+                case 9:
+
+                    int varBusca = 0;
+
+                    System.out.println("Qual valor deseja buscar::");
+                    varBusca = key.nextInt();
+
+                    myObject.busca_Sequencial(varBusca);
+
+                    break;
+                case 10:
+
+                    int chave,resultado;
+
+                    int[] vetortest;
+
+                    vetortest = myObject.getVetor();
+
+                    System.out.println("Qual valor deseja buscar:");
+                    chave = key.nextInt();
+
+                    resultado = myObject.busca_Binaria(myObject.getVetor(),chave,0,vetortest.length-1 );
+
+                    if (resultado == -1) {
+                        System.out.println("Elemento não encontrado:");
+                    } else {
+                        System.out.println("Elemento encontrado no índice: " + resultado);
+                    }
+
+                    break;
+
+                default:
+
+                    System.out.println("idiota");
 
                     break;
             }
--- Estrutura de Dados/Exercício 2/estrutura_OB/src/classeVetor.java
+++ Estrutura de Dados/Exercício 2/estrutura_OB/src/classeVetor.java
@@ -1,38 +1,189 @@
 import java.util.Random;
-import static java.lang.Math.addExact;
+
 
 public class classeVetor {
 
+    int[] vetorInterno;  // definição do vetor interno
 
-    int vetorInterno [];
-
-    classeVetor(int tam){
+    classeVetor(int tam) { // metodo contrutor
 
         vetorInterno = new int[tam];
 
     }
 
-    public int preenchedor_Aleatorio(){
+    public int[] getVetor() {
+        return vetorInterno;
+    }
 
-        int numR, aux;
+    public void preenchedor_Aleatorio() {
+
+        int numR,numR2, aux;
         Random random = new Random();
 
         aux = 0;
 
-        for(int i = 0; i < vetorInterno; i++){
+        for (int i = 0; i < vetorInterno.length; i++) {
 
             aux++;
             numR = random.nextInt(100);
 
-            if(numR == 0 ){
+            if (numR == 0) {
 
-                vetorInterno = addExact(aux,aux-1); //soma inteiro
+                vetorInterno[i] = vetorInterno[numR2 = random.nextInt(100)] + 1;
 
-            }else {
+            } else {
 
-                vetorInterno = numR;
+                vetorInterno[i] = numR;
+
             }
 
 
+        }
+    }  // OK
+
+    public void crecente_Aleatorio() {
+
+            int numR, aux;
+            Random random = new Random();
+
+            aux = 0;
+
+            for(int i = 0; i < vetorInterno.length; i++){
+
+                numR = random.nextInt(100);
+
+
+                if (numR != 0 /*&& numR != vetorInterno[i-1]*/){
+
+                    aux += numR;
+
+                }
+
+                vetorInterno[i] = aux+1;
+
+            }
+
+    }  //OK
+
+    public void elementos_Validos(){
+
+            int flag = 0;
+
+            for(int i = 0; i < vetorInterno.length; i++) {
+
+                if(vetorInterno[i] != 0 ) {
+
+                    flag++;
+
+                }
+
+            }
+
+            System.out.println("Existem " + flag + " valores significativos." );
+
+    } //OK
+
+    public void menor_Maior(){
+
+            int maior,flag,menor;
+
+            maior = vetorInterno[0];
+            flag = 0;
+            for (int i = 0; i < vetorInterno.length; i++) {
+                if (vetorInterno[i] > maior) {
+                    maior = vetorInterno[i];
+                    flag = i;
+                }
+            }
+            System.out.printf("Maior número é %d, no índice %d%n", maior, flag);
+
+            flag = 0;
+            menor = vetorInterno[0];
+
+            for (int i = 0; i < vetorInterno.length; i++) {
+                if (vetorInterno[i] < menor) {
+                    menor = vetorInterno[i];
+                    flag = i;
+                }
+            }
+            System.out.printf("menor número é %d, no índice %d%n8", menor, flag);
+
+    } //OK
+
+    public void info(){
+
+        for (int j : vetorInterno) {
+
+            System.out.printf(" [%d] ", j);
+
+        }
+
+    } //OK
+
+    public void insert_Valor(int valor, int stance){
+
+        vetorInterno[stance] = valor;
+
+    } //OK
+
+    public void apagar_Valor(int apagar){
+
+
+
+            if (apagar != 0 ) {
+
+                for(int i= 0; i < vetorInterno.length; i++) {
+                    if(apagar == vetorInterno[i]){
+                        vetorInterno[i] = 0;
+                    }
+                }
+
+            } else {
+
+                System.out.println("Valor não encontrado ou NULO");
+
+            }
+
+    } //OK
+
+    public void apagar_Indice(int indice){
+
+        vetorInterno[indice] = 0;
+
     }
+
+    public void busca_Sequencial(int varBusca){
+
+        for (int i = 0; i < vetorInterno.length ; i++) {
+
+            if (vetorInterno[i] == varBusca) {
+
+                System.out.printf("Valor %d encontrado no indice %d %n",vetorInterno[i], i );
+
+            }
+        }
+
+    }
+
+
+    public int busca_Binaria (int[] lista, int chave, int esquerda, int direita) {
+
+
+        if (esquerda > direita) {
+            System.out.println("vetor não ordenado idiota");
+            return -1;
+        }
+
+        int meio = (esquerda + direita) / 2;
+
+        if (lista[meio] == chave) {
+            return meio;
+        } else if (chave < lista[meio]) {
+            return busca_Binaria(lista, chave, esquerda, meio - 1);
+        } else {
+            return busca_Binaria(lista, chave, meio + 1, direita);
+        }
+
+    }
+
 }
