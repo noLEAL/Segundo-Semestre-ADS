6abad3cb4bfbbdf17172529efc89845eb6cbeeab
noLEAL <83329725+noLEAL@users.noreply.github.com>
2023-05-20 13:50:18 -0300

    .

---
+++ Programação II/sistema_Livraria.v1/sistema_Livraria.v1/src/produtos.java
@@ -0,0 +1,30 @@
+public class produtos {
+
+    String titulo,editora,area;
+    int codigo,ano,quantidade_em_Estoque;
+    double valor;
+    produtos{
+
+        this.titulo = titulo;
+        this.editora = editora;
+        this.area = area;
+
+        this.ano = ano;
+        this.codigo = codigo;
+        this.quantidade_em_Estoque = quantidade_em_Estoque;
+
+        this.valor = valor;
+
+    }
+
+    public void info(){
+
+        System.out.println(this.codigo);
+        System.out.println(this.titulo , this.editora);
+        System.out.println(this.ca);
+
+
+
+
+    }
+}
